df87bdf598e6a2288cd45af23fb64fcc01a29cf4
Maqsood Ahmed Ansari <106222903+MacAns-117@users.noreply.github.com>
2024-07-01 10:22:00 +0530

    Delete Infinity directory

--- Infinity/infiapp/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-# def home(request):
-#     return render(request,'base.html')
-
-def index(request):
-    return render(request,'index.html')
